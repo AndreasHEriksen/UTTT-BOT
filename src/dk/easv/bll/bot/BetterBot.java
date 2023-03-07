424f2e38527234953acb05a1763bff54b8397093
jonasjakobsen1152 <114471902+jonasjakobsen1152@users.noreply.github.com>
2023-03-07 11:01:52 +0100

    Update BetterBot.java

--- src/dk/easv/bll/bot/BetterBot.java
+++ src/dk/easv/bll/bot/BetterBot.java
@@ -1,37 +1,22 @@
 package dk.easv.bll.bot;
 
+import dk.easv.bll.field.IField;
 import dk.easv.bll.game.GameState;
 import dk.easv.bll.game.IGameState;
 import dk.easv.bll.move.IMove;
+import dk.easv.bll.move.Move;
 
 import java.util.List;
 import java.util.Random;
 
-public class BetterBot implements IBot{
+public class BetterBot implements IBot {
 
     private static final String BOTNAME = "BetterBot";
-    private Random rand = new Random();
+    private Random rand;
 
-    @Override
-    public IMove doMove(IGameState state) {
-        List<IMove> moves = state.getField().getAvailableMoves();
-        GameSimulator simulator = new GameSimulator(state);
-
-
-        if (moves.size() > 0) {
-            return moves.get(rand.nextInt(moves.size())); /* get random move from available moves */
-        }
-        return null;
-    }
-
-    @Override
-    public String getBotName() {
-        return BOTNAME;
-    }
-
-    private GameSimulator createSimulator(IGameState state) {
-        GameSimulator simulator = new GameSimulator(new GameState());
-        simulator.setGameOver(ExampleSneakyBot.GameOverState.Active);
+    private GameSimulator2 createSimulator(IGameState state) {
+        GameSimulator2 simulator = new GameSimulator2(new GameState());
+        simulator.setGameOver(GameOverState.Active);
         simulator.setCurrentPlayer(state.getMoveNumber() % 2);
         simulator.getCurrentState().setRoundNumber(state.getRoundNumber());
         simulator.getCurrentState().setMoveNumber(state.getMoveNumber());
@@ -39,20 +24,49 @@ public class BetterBot implements IBot{
         simulator.getCurrentState().getField().setMacroboard(state.getField().getMacroboard());
         return simulator;
     }
-    class GameSimulator {
+
+    @Override
+    public IMove doMove(IGameState state) {
+        List<IMove> moves = state.getField().getAvailableMoves();
+        GameSimulator2 simulator = createSimulator(state);
+        for (int i = 0; i < 10; i++) {
+            rand = new Random();
+            IGameState gs = simulator.currentState;
+            moves = gs.getField().getAvailableMoves();
+            IMove randomMove = moves.get(rand.nextInt(moves.size()));
+            simulator.updateGame(randomMove);
+            if(moves.size() > 0);
+            {
+                return randomMove;
+            }
+
+        }
+        IMove randomMove = moves.get(rand.nextInt(moves.size()));
+        return randomMove;
+    }
+
+    @Override
+    public String getBotName() {
+        return BOTNAME;
+    }
+
+    public enum GameOverState {
+        Active,
+        Win,
+        Tie
+    }
+
+    class GameSimulator2 {
         private final IGameState currentState;
         private int currentPlayer = 0; //player0 == 0 && player1 == 1
-        private volatile ExampleSneakyBot.GameOverState gameOver = ExampleSneakyBot.GameOverState.Active;
+        private volatile GameOverState gameOver = GameOverState.Active;
 
-        GameSimulator(IGameState currentState) {
-            this.currentState = currentState;
-        }
 
-        public void setGameOver(ExampleSneakyBot.GameOverState state) {
+        public void setGameOver(GameOverState state) {
             gameOver = state;
         }
 
-        public ExampleSneakyBot.GameOverState getGameOver() {
+        public GameOverState getGameOver() {
             return gameOver;
         }
 
@@ -60,9 +74,147 @@ public class BetterBot implements IBot{
             currentPlayer = player;
         }
 
-
         public IGameState getCurrentState() {
             return currentState;
         }
+        GameSimulator2(IGameState currentState) {
+            this.currentState = currentState;
+        }
+
+
+        public boolean updateGame(IMove randomMove) {
+            if (!verifyMoveLegality(randomMove))
+                return false;
+
+            updateBoard(randomMove);
+            currentPlayer = (currentPlayer + 1) % 2;
+
+            return true;
+        }
+        private void updateBoard(IMove move) {
+            String[][] board = currentState.getField().getBoard();
+            board[move.getX()][move.getY()] = currentPlayer + "";
+            currentState.setMoveNumber(currentState.getMoveNumber() + 1);
+            if (currentState.getMoveNumber() % 2 == 0) {
+                currentState.setRoundNumber(currentState.getRoundNumber() + 1);
+            }
+            checkAndUpdateIfWin(move);
+            updateMacroboard(move);
+        }
+        private void updateMacroboard(IMove move) {
+            String[][] macroBoard = currentState.getField().getMacroboard();
+            for (int i = 0; i < macroBoard.length; i++)
+                for (int k = 0; k < macroBoard[i].length; k++) {
+                    if (macroBoard[i][k].equals(IField.AVAILABLE_FIELD))
+                        macroBoard[i][k] = IField.EMPTY_FIELD;
+                }
+
+            int xTrans = move.getX() % 3;
+            int yTrans = move.getY() % 3;
+
+            if (macroBoard[xTrans][yTrans].equals(IField.EMPTY_FIELD))
+                macroBoard[xTrans][yTrans] = IField.AVAILABLE_FIELD;
+            else {
+                // Field is already won, set all fields not won to avail.
+                for (int i = 0; i < macroBoard.length; i++)
+                    for (int k = 0; k < macroBoard[i].length; k++) {
+                        if (macroBoard[i][k].equals(IField.EMPTY_FIELD))
+                            macroBoard[i][k] = IField.AVAILABLE_FIELD;
+                    }
+            }
+        }
+
+        private void checkAndUpdateIfWin(IMove move) {
+            String[][] macroBoard = currentState.getField().getMacroboard();
+            int macroX = move.getX() / 3;
+            int macroY = move.getY() / 3;
+
+            if (macroBoard[macroX][macroY].equals(IField.EMPTY_FIELD) ||
+                    macroBoard[macroX][macroY].equals(IField.AVAILABLE_FIELD)) {
+
+                String[][] board = getCurrentState().getField().getBoard();
+
+                if (isWin(board, move, "" + currentPlayer))
+                    macroBoard[macroX][macroY] = currentPlayer + "";
+                else if (isTie(board, move))
+                    macroBoard[macroX][macroY] = "TIE";
+
+                //Check macro win
+                if (isWin(macroBoard, new Move(macroX, macroY), "" + currentPlayer))
+                    gameOver = GameOverState.Win;
+                else if (isTie(macroBoard, new Move(macroX, macroY)))
+                    gameOver = GameOverState.Tie;
+            }
+        }
+
+        private Boolean verifyMoveLegality(IMove move) {
+            IField field = currentState.getField();
+            boolean isValid = field.isInActiveMicroboard(move.getX(), move.getY());
+
+            if (isValid && (move.getX() < 0 || 9 <= move.getX())) isValid = false;
+            if (isValid && (move.getY() < 0 || 9 <= move.getY())) isValid = false;
+
+            if (isValid && !field.getBoard()[move.getX()][move.getY()].equals(IField.EMPTY_FIELD))
+                isValid = false;
+
+            return isValid;
+        }
+        private boolean isTie(String[][] board, IMove move) {
+            int localX = move.getX() % 3;
+            int localY = move.getY() % 3;
+            int startX = move.getX() - (localX);
+            int startY = move.getY() - (localY);
+
+            for (int i = startX; i < startX + 3; i++) {
+                for (int k = startY; k < startY + 3; k++) {
+                    if (board[i][k].equals(IField.AVAILABLE_FIELD) ||
+                            board[i][k].equals(IField.EMPTY_FIELD))
+                        return false;
+                }
+            }
+            return true;
+        }
+        public boolean isWin(String[][] board, IMove move, String currentPlayer) {
+            int localX = move.getX() % 3;
+            int localY = move.getY() % 3;
+            int startX = move.getX() - (localX);
+            int startY = move.getY() - (localY);
+
+            //check col
+            for (int i = startY; i < startY + 3; i++) {
+                if (!board[move.getX()][i].equals(currentPlayer))
+                    break;
+                if (i == startY + 3 - 1) return true;
+            }
+
+            //check row
+            for (int i = startX; i < startX + 3; i++) {
+                if (!board[i][move.getY()].equals(currentPlayer))
+                    break;
+                if (i == startX + 3 - 1) return true;
+            }
+
+            //check diagonal
+            if (localX == localY) {
+                //we're on a diagonal
+                int y = startY;
+                for (int i = startX; i < startX + 3; i++) {
+                    if (!board[i][y++].equals(currentPlayer))
+                        break;
+                    if (i == startX + 3 - 1) return true;
+                }
+            }
+
+            //check anti diagonal
+            if (localX + localY == 3 - 1) {
+                int less = 0;
+                for (int i = startX; i < startX + 3; i++) {
+                    if (!board[i][(startY + 2) - less++].equals(currentPlayer))
+                        break;
+                    if (i == startX + 3 - 1) return true;
+                }
+            }
+            return false;
         }
     }
+}
